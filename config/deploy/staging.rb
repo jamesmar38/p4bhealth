f053d0e50c304bd67b0d306e530e94f3c9e2bcee
Long Dao <longebane@gmail.com>
2013-08-06 10:42:12 -0700

    WiiiiiPEout

--- config/deploy/staging.rb
+++
@@ -1,9 +0,0 @@
-set :application_path, "staging"
-set :deploy_to, "#{server_root}/#{application_path}"
-set :rails_env, "staging"
-set :branch, "staging"
-
-server "64.207.152.11", :app, :web, :db, :primary => true
-
-set :document_root, "httpdocs"
-set :ee_system, "system"
