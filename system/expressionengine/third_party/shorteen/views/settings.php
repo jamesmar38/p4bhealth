7bf897c7832598367e7f9fb40f9d761bc95e8941
Mike Stecker <mike@stecker.me>
2013-08-23 10:02:58 -0700

    removing unused addons

--- system/expressionengine/third_party/shorteen/views/settings.php
+++
@@ -1,8 +0,0 @@
-<?=form_open('C=addons_modules'.AMP.'M=show_module_cp'.AMP.'module=shorteen'.AMP.'method=save_settings');?>
-
-<?=$providers?>
-
-<p><?=form_submit('submit', lang('save'), 'class="submit"')?></p>
-
-<?php
-form_close();
