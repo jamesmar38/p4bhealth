0e39974b174cf6728c3dce710d4cb6c730333afc
Mike Stecker <mike@stecker.me>
2014-03-26 20:50:20 -0700

    updating git and server location

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ set :deploy_to, "#{server_root}/#{application_path}"
 set :rails_env, "production"
 set :branch, "production"
 
-server "64.207.152.11", :app, :web, :db, :primary => true
+server "64.207.182.180", :app, :web, :db, :primary => true
 
 set :document_root, "httpdocs"
 set :ee_system, "system"
