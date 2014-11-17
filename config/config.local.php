1b424beca421588b6915d7527afc5b9931253e25
Mike Stecker <mike@stecker.me>
2014-11-17 10:10:44 -0800

    local db

--- config/config.local.php
+++ config/config.local.php
@@ -15,7 +15,7 @@
 // Local db
 $env_db['hostname'] = '127.0.0.1';
 $env_db['username'] = 'root';
-$env_db['password'] = 'asdf';
+$env_db['password'] = 'root';
 $env_db['database'] = 'pbh_ee';
 
 /**
