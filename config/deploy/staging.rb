1792ea2afa46a9d7e2f332df17851162fc0a9d4a
Mike Stecker <mike@stecker.me>
2014-11-17 10:09:05 -0800

    Updating staging deploy location

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@ set :deploy_to, "#{server_root}/#{application_path}"
 set :rails_env, "staging"
 set :branch, "staging"
 
-server "64.207.182.180", :app, :web, :db, :primary => true
+server "162.243.130.172:5932", :app, :web, :db, :primary => true
 
 set :document_root, "httpdocs"
 set :ee_system, "system"
